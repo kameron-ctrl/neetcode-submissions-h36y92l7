78415fa4119ba3d63d840cf47c683280332167d1
kameron-ctrl <79770549+kameron-ctrl@users.noreply.github.com>
2026-04-08 03:51:32 -0400

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        newstring = ''
+        for i in s:
+            if i.isalnum():
+                newstring += i.lower()
+        return newstring == newstring[::-1]
